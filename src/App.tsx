6e75a6673a58cadcc2d3244a5a1070235725e39f
mohankrish1 <mohankrishna13012001@gmail.com>
2024-06-12 21:58:06 +0530

    day2 done

--- src/App.tsx
+++ src/App.tsx
@@ -1,35 +1,25 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+import { useState } from 'react';
+import './App.css';
+import { createClient } from '@supabase/supabase-js'
+
+const supabaseUrl = 'https://urwdvetirkafraxpsnif.supabase.co/';
+const supabaseKey = 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6InVyd2R2ZXRpcmthZnJheHBzbmlmIiwicm9sZSI6ImFub24iLCJpYXQiOjE3MTgxODA4ODEsImV4cCI6MjAzMzc1Njg4MX0.eyKC0o6E6Xz3rj6WBFoyyw6s0qdjyou_Jw1A9sBM3UY';
+const supabase = createClient(supabaseUrl, supabaseKey);
 
 function App() {
-  const [count, setCount] = useState(0)
+  const [phone,setPhone] = useState<string>('');
 
-  return (
-    <>
-      <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.tsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-    </>
-  )
+  const getData = async(phone : string) => {
+    const { data, error } = await supabase.auth.signInWithOtp({
+      phone:`+${phone}`
+    });
+    console.log(data,error);
+  };
+
+  return <>
+  <input type="text" onChange={(e:React.ChangeEvent<HTMLInputElement>)=> setPhone(e.target.value)} />
+  <button onClick={()=>getData(phone)}>Send OTP</button>
+  </>;
 }
 
-export default App
+export default App;
