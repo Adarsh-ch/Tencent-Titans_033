80163be951a6f2506c2202ad4dd43dceb8a46a74
Adarsh-ch <adarshchaturvedi98@gmail.com>
2024-06-14 15:32:06 +0530

    Done header part

--- src/main.tsx
+++ src/main.tsx
@@ -4,6 +4,8 @@ import App from './App.tsx'
 import './index.css'
 import AuthProvider from './context/AuthContext.tsx'
 import 'bootstrap/dist/css/bootstrap.min.css';
+import 'bootstrap/dist/js/bootstrap.bundle.min.js';
+
 
 ReactDOM.createRoot(document.getElementById('root')!).render(
   <React.StrictMode>
