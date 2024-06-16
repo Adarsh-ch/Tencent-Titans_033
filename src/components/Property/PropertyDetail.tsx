90dfeb519585b743f6fb6edc16e59affca9e49b4
aniyant <kumarsunny30066@gmail.com>
2024-06-16 16:04:09 +0530

    propertyDetails

--- src/components/Property/PropertyDetail.tsx
+++ src/components/Property/PropertyDetail.tsx
@@ -1,10 +1,11 @@
 
-
-
 import React from 'react';
 import { Property } from '../../types';
-import '../../styles/PropertyDetail.css';
+// import '../../styles/PropertyDetail.css';
 import Footer from '../common/Footer';
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import { faBuildingCircleExclamation, faChartArea, faHouseChimneyCrack, faLocationDot, faPaintRoller, faPersonCircleQuestion, faR, faTreeCity } from '@fortawesome/free-solid-svg-icons';
+import { Box, ChakraProvider, Flex, Heading, Image, Text, VStack,Button } from '@chakra-ui/react';
 
 interface PropertyDetailProps {
   property: Property;
@@ -13,37 +14,46 @@ interface PropertyDetailProps {
 const PropertyDetail: React.FC<PropertyDetailProps> = ({ property }) => {
   // Mapping property fields to icons
   const propertyDetails = [
-    { icon: '📍', label: 'Location', text: property.location },
-    { icon: '💵', label: 'Rent', text: property.rent },
-    { icon: '🛠️', label: 'Maintenance', text: property.maintenance },
-    { icon: '📐', label: 'Area', text: property.area },
-    { icon: '🛋️', label: 'Furniture Type', text: property.furniture_type },
-    { icon: '🏢', label: 'Flat Type', text: property.flat_type },
-    { icon: '👥', label: 'Preferred Category', text: property.prefer_category },
-    { icon: '🏠', label: 'Property Type', text: property.property_type },
+    { icon:<FontAwesomeIcon icon={faLocationDot} />, label: 'Location', text: property.location },
+    { icon:<FontAwesomeIcon icon={faR}/>, label: 'Rent', text: property.rent },
+    { icon: <FontAwesomeIcon icon={faPaintRoller}/>, label: 'Maintenance', text: property.maintenance },
+    { icon: <FontAwesomeIcon icon={faChartArea} />, label: 'Area', text: property.area },
+    { icon:<FontAwesomeIcon icon={faHouseChimneyCrack} />, label: 'Furniture Type', text: property.furniture_type },
+    { icon:<FontAwesomeIcon icon={faTreeCity} />, label: 'Flat Type', text: property.flat_type },
+    { icon:<FontAwesomeIcon icon={faPersonCircleQuestion} />, label: 'Preferred Category', text: property.prefer_category },
+    { icon: <FontAwesomeIcon icon={faBuildingCircleExclamation} />, label: 'Property Type', text: property.property_type },
   ];
 
   return (
     <>
-    <div className="property-detail">
-      <img src={property.image} alt={property.title} />
-      <div>
-        <h1 className='title'>{property.title}</h1>
-        <h4 className='description'>{property.description}</h4>
-        <div className="table">
-          {propertyDetails.map((detail, index) => (
-            <div key={index} className="table-row">
-              <span className="icon">{detail.icon}</span>
-              <span className="label">{detail.label}</span>
-              <span className="text">{detail.text}</span>
-            </div>
-            
-          ))}
-             <button className='btn-owner'>Add to Wishlist</button>
-        </div>
-     
-      </div>
-    </div>
+    <ChakraProvider>
+      <Box as="div" maxWidth={"1440px"} m="auto">
+        <Box textAlign="center">
+          <Text as="h3">{property.title}</Text>
+          <Text as="h5">{property.description}</Text>
+        </Box>
+        <Flex flexDir={['column','column','row']} justifyContent="center" alignItems="center" gap={50}>
+          <Box>
+            <Image src={property.image} alt={property.title} />
+          </Box>
+          <VStack spacing={3} textAlign="center">
+            {propertyDetails.map((detail, index) => (
+              <Box width="100%" key={index} className="table-row" textAlign="left">
+                <Text as="span" className="icon">{detail.icon}</Text>
+                <Text as="span" className="label">{detail.label}</Text>
+                <Text as="span" className="text">{detail.text}</Text>
+              </Box>
+            ))}
+            <Flex width="100%" >
+                <Button>Contact Owner</Button>
+                <Button>Add to Wishlist</Button>
+                <Button>Report</Button>
+            </Flex>
+          </VStack>
+        </Flex>
+
+      </Box>
+    </ChakraProvider>
     <Footer/>
     </>
   );
--- src/pages/PropertyDetailPage.tsx
+++ src/pages/PropertyDetailPage.tsx
@@ -5,23 +5,11 @@ import { Property } from '../types';
 
 const PropertyDetailPage: React.FC = () => {
   const { id } = useParams<{ id: string }>();
-  console.log(id,typeof id);
   const [property, setProperty] = React.useState<Property | null>(null);
   const [error, setError] = React.useState<string | null>(null);
 
   React.useEffect(() => {
     const fetchProperty = async () => {
-      // if (!id) {
-      //   setError('Invalid property ID');
-      //   return;
-      // }
-
-      // const propertyId = parseInt(id, 10);
-      // if (isNaN(propertyId)) {
-      //   setError('Property ID is not a valid number');
-      //   return;
-      // }
-
       try {
         const response = await fetch(`http://localhost:5001/properties/${id}`);
         console.log(`http://localhost:5001/properties/`+id);
@@ -36,9 +24,7 @@ const PropertyDetailPage: React.FC = () => {
     };
 
     fetchProperty();
-  }, [id]);
-
-  console.log(property);
+  }, []);
 
   if (error) {
     return <div>Error: {error}</div>;
